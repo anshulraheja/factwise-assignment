0378d8131b8c995c3d5327aa71e6d04d2aee912a
Anshul Raheja <40492015+anshulraheja@users.noreply.github.com>
2023-07-02 14:09:59 +0000

    add data folder

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,7 @@ import Accordion from "./components/Accordion";
 import Modal from "./components/Modal";
 import { Person } from "./utils/helpers";
 import "./styles.css";
-
-const data: Person[] = [
-  {
-    id: 1,
-    name: "John Doe",
-    DOB: "1990-01-01",
-    gender: "male",
-    country: "India",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam efficitur sapien nec risus vulputate, sit amet sollicitudin lacus aliquet. Suspendisse lobortis feugiat lobortis. Nam vel varius nunc. Suspendisse in mi turpis. Suspendisse elementum imperdiet elit",
-  },
-  {
-    id: 2,
-    name: "Alice",
-    DOB: "1991-01-01",
-    gender: "male",
-    country: "India",
-    description:
-      "Vestibulum congue mattis enim, sit amet convallis lectus faucibus sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at nulla sapien. Sed eget est a justo dignissim efficitur. Proin pretium rhoncus molestie. Sed feugiat",
-  },
-  {
-    id: 3,
-    name: "Annie J",
-    DOB: "1992-01-01",
-    gender: "female",
-    country: "USA",
-    description:
-      "Integer vel ultrices ex. Sed vel nulla neque. Sed accumsan lacinia risus congue mollis. Duis vitae consectetur nisl, nec ornare mi. Sed non pulvinar urna, a varius turpis.",
-  },
-];
+import {data} from './utils/data';
 
 const App: React.FC = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -56,6 +27,8 @@ const App: React.FC = () => {
   const closeModal = () => {
     setPersonToDelete(null);
   };
+  
+  
 
   return (
     <div className="app">
---
+++ src/utils/data.tsx
@@ -0,0 +1,32 @@
+import {Person} from './helpers';
+
+
+export const data: Person[] = [
+  {
+    id: 1,
+    name: "John Doe",
+    DOB: "1990-01-01",
+    gender: "male",
+    country: "India",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam efficitur sapien nec risus vulputate, sit amet sollicitudin lacus aliquet. Suspendisse lobortis feugiat lobortis. Nam vel varius nunc. Suspendisse in mi turpis. Suspendisse elementum imperdiet elit",
+  },
+  {
+    id: 2,
+    name: "Alice",
+    DOB: "1991-01-01",
+    gender: "male",
+    country: "India",
+    description:
+      "Vestibulum congue mattis enim, sit amet convallis lectus faucibus sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at nulla sapien. Sed eget est a justo dignissim efficitur. Proin pretium rhoncus molestie. Sed feugiat",
+  },
+  {
+    id: 3,
+    name: "Annie J",
+    DOB: "1992-01-01",
+    gender: "female",
+    country: "USA",
+    description:
+      "Integer vel ultrices ex. Sed vel nulla neque. Sed accumsan lacinia risus congue mollis. Duis vitae consectetur nisl, nec ornare mi. Sed non pulvinar urna, a varius turpis.",
+  },
+];
